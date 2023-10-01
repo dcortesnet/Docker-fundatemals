23794a545159314e75a632c91333aadb85ea4853
dcortesnet <dcortes.net@gmail.com>
2023-10-01 10:50:23 -0300

    Run commands of apache and nginx

---
+++ docker-runs/apache/command.sh
@@ -0,0 +1,3 @@
+docker run -d -p 80:80 --name my-apache httpd
+docker stop my-apache
+docker rm my-apache
---
+++ docker-runs/nginx/command.sh
@@ -0,0 +1,3 @@
+docker run -d -p 80:80 --name my-nginx nginx
+docker stop my-nginx
+docker rm my-nginx
